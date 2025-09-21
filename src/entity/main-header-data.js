6befc1d04b1d446b07085250e44211070d5e8a3a
Yeonu-Kim <ywk0524@snu.ac.kr>
2025-09-21 17:22:50 +0900

    ✨ 호버 시 알림 모달 나타나는 기능 추가

--- src/entity/main-header-data.js
+++ src/entity/main-header-data.js
@@ -16,3 +16,48 @@ export const HEADER_DATA = {
     { contents: "언어별로 찾아보기", href: ROUTE.LANGUAGE_SEARCH },
   ],
 };
+
+export const HEADER_NOTIFICATION = [
+  {
+    id: "notification-1",
+    title: "은중과상연",
+    contents: "9월 12일 공개",
+    time: "오늘",
+    unread: true,
+  },
+  {
+    id: "notification-2",
+    title: "넷플릭스 '신규 콘텐츠 가이드'",
+    contents: "공개 예정작을 살펴보세요.",
+    time: "4일",
+    unread: false,
+  },
+  {
+    id: "notification-3",
+    title: "신규 콘텐츠",
+    contents: "나는 생존자다",
+    time: "3주 전",
+    unread: true,
+  },
+  {
+    id: "notification-4",
+    title: "손맛이 다른 세계가 온다",
+    contents: "《웬즈데이》 시즌 2 공개",
+    time: "4주 전",
+    unread: true,
+  },
+  {
+    id: "notification-5",
+    title: "신규 콘텐츠",
+    contents: "에스콰이어: 변호사를 꿈꾸는 변호사들",
+    time: "1개월",
+    unread: true,
+  },
+  {
+    id: "notification-6",
+    title: "대한민국의 TOP 10 시리즈",
+    contents: "인기 콘텐츠를 확인해 보세요.",
+    time: "1개월",
+    unread: true,
+  },
+];
